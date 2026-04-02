65ea30b1839f271e72434664ac4a32ca7dc4af9c
Vu Dinh Phong <vdphong_26_11@outlook.com>
2026-04-02 16:51:38 +0700

    Add `test_load_requestsPhotosRetrieval`

--- MemapData/MemapDataTests/MemapCache/LoadPhotosFromPersistentStorageUseCaseTests.swift
+++ MemapData/MemapDataTests/MemapCache/LoadPhotosFromPersistentStorageUseCaseTests.swift
@@ -7,12 +7,18 @@
 
 import XCTest
 
+struct Photo {}
+
 class PhotosStoreSpy {
     enum ReceivedMessage: Equatable {
         case retrieve
     }
     
     private(set) var receivedMessages: [ReceivedMessage] = []
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
+    }
 }
 
 class PhotosPersistentLoader {
@@ -21,16 +27,40 @@ class PhotosPersistentLoader {
     init(store: PhotosStoreSpy) {
         self.store = store
     }
+    
+    func load(from photoPath: String, completion: @escaping ([Photo]) -> Void) {
+        store.retrieve()
+    }
 }
 
 final class LoadPhotosFromPersistentStorageUseCaseTests: XCTestCase {
     
     func test_init_deliversNoMessageUponCreation() {
-        
-        let store = PhotosStoreSpy()
-        let _ = PhotosPersistentLoader(store: store)
+        let (_, store) = makeSUT()
         
         XCTAssertEqual(store.receivedMessages, [])
     }
-
+    
+    func test_load_requestsPhotosRetrieval() {
+        let (sut, store) = makeSUT()
+        let path = "anypath"
+        
+        sut.load(from: path) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    // MARK: - Helpers
+    
+    
+    private func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: PhotosPersistentLoader, store: PhotosStoreSpy) {
+        let store = PhotosStoreSpy()
+        let sut = PhotosPersistentLoader(store: store)
+        
+        return (sut, store)
+    }
+    
 }
