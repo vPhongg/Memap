e818757c5bfbc9553c9e1fe7ea34bebd8f58583f
Vu Dinh Phong <vdphong_26_11@outlook.com>
2026-04-02 18:37:48 +0700

    Add `test_load_failedOnRetrievalError`

--- MemapData/MemapDataTests/MemapCache/LoadPhotosFromPersistentStorageUseCaseTests.swift
+++ MemapData/MemapDataTests/MemapCache/LoadPhotosFromPersistentStorageUseCaseTests.swift
@@ -7,16 +7,19 @@
 
 import XCTest
 
-struct Photo {}
-
 class PhotosStoreSpy {
+    typealias RetrievalCompletion = (Error) -> Void
+    
     enum ReceivedMessage: Equatable {
         case retrieve
     }
     
     private(set) var receivedMessages: [ReceivedMessage] = []
     
-    func retrieve() {
+    var retrievalCompletions = [RetrievalCompletion]()
+    
+    func retrieve(from photosPath: String, completion: @escaping RetrievalCompletion) {
+        retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
     }
 }
@@ -28,8 +31,8 @@ class PhotosPersistentLoader {
         self.store = store
     }
     
-    func load(from photoPath: String, completion: @escaping ([Photo]) -> Void) {
-        store.retrieve()
+    func load(from photosPath: String, completion: @escaping (Error) -> Void) {
+        store.retrieve(from: photosPath, completion: completion)
     }
 }
 
@@ -43,15 +46,36 @@ final class LoadPhotosFromPersistentStorageUseCaseTests: XCTestCase {
     
     func test_load_requestsPhotosRetrieval() {
         let (sut, store) = makeSUT()
-        let path = "anypath"
         
-        sut.load(from: path) { _ in }
+        sut.load(from: anyPhotosPath()) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    func test_load_failedOnRetrievalError() {
+        let (sut, store) = makeSUT()
+        
+        let expectation = expectation(description: "Waiting for completion to be invoke")
+        
+        sut.load(from: anyPhotosPath()) { error in
+            
+            XCTAssertEqual(error as NSError, anyNSError())
+            
+            expectation.fulfill()
+        }
+        
+        store.retrievalCompletions[0](anyNSError())
+        
+        wait(for: [expectation], timeout: 1.0)
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
     // MARK: - Helpers
     
+    private func anyPhotosPath() -> String {
+        return "any/photos/path"
+    }
     
     private func makeSUT(
         file: StaticString = #filePath,
