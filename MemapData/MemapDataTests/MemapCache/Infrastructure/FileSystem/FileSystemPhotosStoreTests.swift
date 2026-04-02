bb1b1c045f7d51d5c372abf7589ac463e2d34405
Vu Dinh Phong <vdphong_26_11@outlook.com>
2026-04-02 22:31:21 +0700

    Create empty `FileSystemPhotosStoreTests` file

---
+++ MemapData/MemapDataTests/MemapCache/Infrastructure/FileSystem/FileSystemPhotosStoreTests.swift
@@ -0,0 +1,12 @@
+//
+//  FileSystemPhotosStoreTests.swift
+//  MemapDataTests
+//
+//  Created by Vu Dinh Phong on 02/04/2026.
+//
+
+import XCTest
+
+final class FileSystemPhotosStoreTests: XCTestCase {
+
+}
