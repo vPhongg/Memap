da0395ca839669841c580456de30274db352d86e
Vu Dinh Phong <vdphong_26_11@outlook.com>
2026-05-02 00:33:34 +0700

    Move item to it owns file

--- MemapUI/MemapUI/PlaceDetailView/SubViews/CollectionItemView.swift
+++ MemapUI/MemapUI/PlaceDetailView/SubViews/CollectionItemView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct CollectionItemView: View {
+    private static let size: CGFloat = 100.0
+    private static let spacing: CGFloat = 12.0
+    private static let cornerRadius: CGFloat = 12.0
+    
     let image: UIImage
     
     var body: some View {
         Image(uiImage: image)
+            .resizable()
+            .scaledToFill()
+            .frame(width: CollectionItemView.size, height: CollectionItemView.size)
+            .clipped()
+            .cornerRadius(CollectionItemView.cornerRadius)
     }
 }
--- MemapUI/MemapUI/PlaceDetailView/SubViews/CollectionView.swift
+++ MemapUI/MemapUI/PlaceDetailView/SubViews/CollectionView.swift
@@ -12,20 +12,14 @@ struct CollectionView: View {
     
     let images: [UIImage]
     
-    private static let size: CGFloat = 100.0
-    private static let spacing: CGFloat = 12.0
-    private static let cornerRadius: CGFloat = 12.0
-    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack() {
                 ForEach(images, id: \.self) { image in
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: CollectionView.size, height: CollectionView.size)
-                        .clipped()
-                        .cornerRadius(CollectionView.cornerRadius)
+                    CollectionItemView(image: image)
+                        .onTapGesture {
+                            print("Image tapped")
+                        }
                 }
             }
         }
